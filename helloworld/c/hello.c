7d61efc224459740b6dc09cf3c9ab5c09c703f73
rocedu <rocedu@163.com>
2016-04-07 21:00:47 +0800

    hello world c

---
+++ helloworld/c/hello.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2016/04/07 20时56分12秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  rocedu (http://www.cnblogs.com/rocedu/), rocedu@163.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+int main(){
+	printf("Hello World!\n");
+	return 0;
+}
+
