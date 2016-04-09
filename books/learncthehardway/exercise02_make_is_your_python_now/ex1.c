ceb754699dc3a99acb342beda066b42754658ed7
rocedu <rocedu@163.com>
2016-04-09 21:14:15 +0800

    Makefile test

---
+++ books/learncthehardway/exercise02_make_is_your_python_now/ex1.c
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ex1.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2016/04/09 20时51分41秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  rocedu (http://www.cnblogs.com/rocedu/), rocedu@163.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+
+#include	<stdio.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+	int
+main ( int argc, char *argv[] )
+{
+	puts("hello world!");
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
