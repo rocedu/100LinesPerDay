6cb645f6ef3617f68cf3a95686a8dae64a981bf7
rocedu <rocedu@163.com>
2016-04-09 22:38:57 +0800

    python hello world and print

---
+++ books/learnpythonthehardway/exercise01_a_good_first_program/ex1.py
@@ -0,0 +1,7 @@
+print "hello world!"
+print "hello again!"
+print "I like type this."
+print "This is fun!"
+print "Yay, printing!"
+print "I'd much rather you  'not'."
+print 'I " said " do not touch this.'
