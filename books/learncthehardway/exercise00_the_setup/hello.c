a93d9babdc8395bf1a7805e9a0f26033532edc9d
rocedu <rocedu@163.com>
2016-04-09 20:48:28 +0800

    hello with c.vim

---
+++ books/learncthehardway/exercise00_the_setup/hello.c
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2016/04/09 20时41分58秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  rocedu (http://www.cnblogs.com/rocedu/), rocedu@163.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include	<stdlib.h>
+
+#include <stdio.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+	int
+main ( int argc, char *argv[] )
+{
+	printf("Hello World!\n");
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
+
