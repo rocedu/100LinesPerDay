f9a06da7b6468bcf6447d144ecc0f3c3dc5c07fb
rocedu <rocedu@163.com>
2016-05-08 13:56:34 +0800

    printf format

---
+++ books/learncthehardway/exercise03_formatted_printing/ex3.c
@@ -0,0 +1,15 @@
+#include	<stdlib.h>
+#include	<stdio.h>
+
+int main ( int argc, char *argv[] )
+{
+
+	int age = 9;
+	int height = 72;
+
+	printf("I'm %d years old. \n", age);
+	printf("I'm %d inches tall. \n", height);
+
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
+
