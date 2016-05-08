2a629bad11d4b4b989f4e93d31354fa7fdbe5468
rocedu <rocedu@163.com>
2016-05-08 14:40:56 +0800

    valgrind test

---
+++ books/learncthehardway/exercise04_introducing_valgrind/ex4.c
@@ -0,0 +1,15 @@
+#include	<stdlib.h>
+#include	<stdio.h>
+
+int main ( int argc, char *argv[] )
+{
+
+	int age = 10;
+	int height;
+
+	printf("I'm %d years old. \n");
+	printf("I'm %d inches tall. \n", height);
+
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
+
